cdb2dacf8300bcedfb991527595d655462117e05
Ian Guthrie <IGuthrie@aol.com>
1998-08-08 22:51:27 +0000

    Import of IGUTHRIE/Filesys-Statvfs_Df-0.47 from CPAN.
    gitpan-cpan-distribution: Filesys-Statvfs_Df
    gitpan-cpan-version:      0.47
    gitpan-cpan-path:         IGUTHRIE/Filesys-Statvfs_Df-0.47.tar.gz
    gitpan-cpan-author:       IGUTHRIE
    gitpan-cpan-maturity:     released

--- Df.pm
+++ Df.pm
@@ -10,7 +10,7 @@ require Exporter;
 
 @ISA = qw(Exporter);
 @EXPORT = qw(df);
-$VERSION = '0.46';
+$VERSION = '0.47';
 
 sub df {
 my ($dir, $block_size)=@_;
--- Statvfs.pm
+++ Statvfs.pm
@@ -8,7 +8,7 @@ require DynaLoader;
 
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw(statvfs);
-$VERSION = '0.46';
+$VERSION = '0.47';
 bootstrap Filesys::Statvfs $VERSION;
 
 1;
